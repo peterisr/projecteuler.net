1581f9494524ff02a11c06414e981ef085cd34a8
Peteris Rudzusiks <peteris.rudzusiks@gmail.com>
2016-09-21 15:13:05 +0200

    Delete all binaries in cleanup script.

--- cleanup.sh
+++ cleanup.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 find -name *.o -delete
+find . -type f | xargs file | grep "ELF.*executable" | awk -F: '{print $1}' | xargs rm -v
 if [ -f projecteuler_test ]; then 
 	rm projecteuler_test
 fi;
